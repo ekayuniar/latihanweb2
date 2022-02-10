08d6f365f0f51b93ca583832213bfbc1854d622a
eka yuniar <sayayuniar@gmail.com>
2022-02-10 19:50:13 +0700

    update aritmatika

---
+++ aritmatika.php
@@ -0,0 +1,23 @@
+<?php
+$x = 10;
+$y = 22;
+
+// penjumlahan
+echo "jumlah dari variable x dan y adalah ";
+echo $x + $y;
+echo "<br>";
+// pengurangan
+echo "jumlah dari variable x dikurang y adalah ";
+echo $x - $y;
+echo "<br>";
+// perkalian
+echo "jumlah dari variable x dikali y adalah ";
+echo $x * $y;
+echo "<br>";
+// Pembagian
+echo "jumlah dari variable x dibagi y adalah ";
+echo $x / $y;
+echo "<br>";
+// Modus
+echo "jumlah modus dari variable y dan x adalah ";
+echo $y % $x;
